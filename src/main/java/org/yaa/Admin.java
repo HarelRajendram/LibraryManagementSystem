04302e263c47c41d1e5757562c54d962f0d5d181
HarelRajendram <2534297@edu.vaniercollege.qc.ca>
2026-05-13 22:28:38 -0400

    finished the admin class

--- src/main/java/org/yaa/Admin.java
+++ src/main/java/org/yaa/Admin.java
@@ -10,10 +10,20 @@ public class Admin extends User implements Reportable {
     public int getBorrowLimit() {
         return Integer.MAX_VALUE;
     }
-    public void backupData() throws LibraryException {
-
+    public void backupData(Library library) throws LibraryException {
+        library.backupData();
     }
     public String generateReport() {
-        return "Report not implemented but soon";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Admin report").append(userName).append("/n");
+        sb.append("Borrowed Items: /n");
+
+        for (Item item : borrowedItems) {
+            sb.append("-").append(item.getItemId())
+                    .append("(").append(item.getItemStatus())
+                    .append(")");
+        }
+        return sb.toString();
+
     }
 }
